c80cbbc4d130a118e8a75355bf71f8ca3e0e35d5
Murilo Gustavo Nabarrete Costa <murilognc@hotmail.com.br>
2018-11-27 18:07:12 -0300

    Sou o cara

--- Front/HTML/navbar_menulateral.php
+++ Front/HTML/navbar_menulateral.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Brinque Feliz</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
