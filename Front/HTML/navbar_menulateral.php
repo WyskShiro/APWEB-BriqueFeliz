a971c0e0cd956a8a86d2b429a3ce5812ff2427d1
Murilo Gustavo Nabarrete Costa <murilognc@hotmail.com.br>
2018-11-29 12:28:55 -0300

    Correção menu inicial e criação de arquivos

--- Front/HTML/navbar_menulateral.php
+++ Front/HTML/navbar_menulateral.php
@@ -1,44 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Brinque Feliz</title>
+    
+    <div class="container bg-warning text-dark">
+        <div class="btn-group-vertical" role="group" aria-label="Opções menu">
+            <button type="button" class="btn btn-secondary">Carrinho de compras</button>
+            <button type="button" class="btn btn-secondary">Produtos</button>
+            <button type="button" class="btn btn-secondary">Funcionários</button>
+            <button type="button" class="btn btn-secondary">Clientes</button>
+            <button type="button" class="btn btn-secondary">Fornecedores</button>
+            <button type="button" class="btn btn-secondary">Relatórios</button>
+        </div>
+    </div>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-    <div class="container">
-
-        <header>
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="">Brinque Feliz</a>
-                    </div>
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Ajuda</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-
-        <aside class="col-xs-2">
-            <ul>
-                <li><a>Carrinho</a></li>
-                <li><a>Caixinha</a></li>
-                <li><a>Duas pessoas</a></li>
-                <li><a>Funcionário</a></li>
-            </ul>
-        </aside>
+</body>
--- Front/HTML/tela_opcoes.php
+++ Front/HTML/tela_opcoes.php
@@ -1,16 +1,73 @@
-<?php include_once('navbar_menulateral.php') ?>
 
-<section>
-    <h2>Acesso Rápido</h2>
+<!DOCTYPE html>
+<html lang="en">
 
-    <button>Personalizar</button>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Brinque Feliz</title>
 
-    <a href=""></a>
-</section>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Brinque Feliz</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ajuda</a>
+            </li>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
+            </ul>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Olá,XXXXX</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Realizar Venda</a>
+                <a class="dropdown-item" href="#">Outra ação</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Sair</a>
+                </div>
+            </li>
+        </div>
+    </nav>
+    </div>
+
+    <div class="container-fluid">
+        <div class="btn-group-vertical" role="group" aria-label="Opções menu">
+            <button type="button" class="btn btn-secondary">Carrinho de compras</button>
+            <button type="button" class="btn btn-secondary">Produtos</button>
+            <button type="button" class="btn btn-secondary">Funcionários</button>
+            <button type="button" class="btn btn-secondary">Clientes</button>
+            <button type="button" class="btn btn-secondary">Fornecedores</button>
+            <button type="button" class="btn btn-secondary">Relatórios</button>
+        </div>
+    </div>
 
 
 
-</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
-
 </html>
