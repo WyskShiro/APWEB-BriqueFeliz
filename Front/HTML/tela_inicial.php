7be198adc288ffc2d6dc646b46cbed1c081a85bb
Rhebeca Abreu <rhebecaabreu@gmail.com>
2018-12-01 01:27:27 -0300

    Testando codigo template bootstrap

--- Front/HTML/tela_inicial.php
+++ Front/HTML/tela_inicial.php
@@ -5,38 +5,87 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="main.css" rel="stylesheet">
+
     <title>Brinque Feliz</title>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="row justify-content-center">
+        <!-- <div class="row">
+            <div class="col"> -->
+                <!-- <div class="row justify-content-center">
                     <div class="col-sm-6">
                         <h1>Brinque Feliz</h1>
                         <h2>Era uma vez uma grande loja...</h2>
                     </div>
-                </div>
+                </div> -->
 
-                <div class="row justify-content-center">
-                    <div class="col-6 col-sm-6">
+                <!-- <div class="row justify-content-center">
+                    <div class="col-6 col-sm-6"> -->
                         <!--
                         FORMULÁRIO PARA LOGAR
                             -->
-                        <div class="card" style="width: 18rem;">
+                        <!-- <div class="card" style="width: 18rem;">
                             <div class="card-body">
                                 <h5 class="card-title text-center">Entrar</h5>
                                 <form class="form-inline" method="post">
-                            <input type="hidden" name="funcao" value="realizarLogin">
+                                    <input type="hidden" name="funcao" value="realizarLogin">
+
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">@</div>
+                                        </div>
+                                        <label for="numeroId"><span class="glyphicon glyphicon-user"></span></label>
+                                        <input type="text" class="form-control" name="numeroId" id="numeroId"
+                                            placeholder="Número de Identificação">
+                                    </div>
+
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">@</div>
+                                        </div>
+                                        <label for="cpf"><span class="glyphicon glyphicon-lock"></span></label>
+                                        <input type="text" class="form-control" name="cpf" id="cpf" placeholder="CPF">
+                                    </div>
+
+                                    <input type="submit" class="btn btn-primary btn-block" value="Entrar">
+                                </form>
+                            </div>
+                        </div> -->
+<!-- 
+                    </div>
+                </div>
+            </div> -->
+
+            <!-- <div class="col">
+                <div class="col-sm-6">
+                    <img src="Recursos/child_playing.jpg" class="img-fluid " alt="IMAGEM DO BRINQUE FELIZ">
+                </div>
+            </div> -->
+        <!-- </div> -->
+
+        <div class="row justify-content-center">
+            <div class="d-md-flex flex-md-equal my-md-3 pl-md-3 justify-content-center w-100 h-100">
+
+                <div class="bg-light mr-md-3 px-md-5 text-center overflow-hidden ">
+                    <div class="my-3 py-3">
+                        <h2 class="display-5">Brinque Feliz</h2>
+                        <p class="lead">Era uma vez uma grande loja...</p>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Entrar</h5>
+                            <form class="form-inline" method="post">
+                                <input type="hidden" name="funcao" value="realizarLogin">
 
                                 <div class="input-group mb-2">
                                     <div class="input-group-prepend">
@@ -54,28 +103,29 @@
                                     <input type="text" class="form-control" name="cpf" id="cpf" placeholder="CPF">
                                 </div>
 
-                            <input type="submit" class="btn btn-primary btn-block" value="Entrar">
-                        </form>
+                                <input type="submit" class="btn btn-primary btn-block" value="Entrar">
+                            </form>
+                        </div>
                     </div>
                 </div>
 
-                        <!----------------------------->
-                    </div>
+                <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                        <img src="Recursos/child_playing.jpg" class="img-fluid " alt="IMAGEM DO BRINQUE FELIZ">
                 </div>
-            </div>
 
-            <div class="col">
-                <div class="col-sm-6">
-                   <img src="Recursos/child_playing.jpg" class="img-fluid" alt="IMAGEM DO BRINQUE FELIZ">
-                </div>
             </div>
         </div>
+
+
     </div>
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 
 </body>
 
