aaa0f80e65bc9d27dcc6dab66cba417478463e2f
Rhebeca Abreu <rhebecaabreu@gmail.com>
2018-12-01 15:33:36 -0300

    Primeira versão tela inicial finalizada

--- Front/HTML/tela_inicial.php
+++ Front/HTML/tela_inicial.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="main.css" rel="stylesheet">
+    <link href="main.css" type="text/css" rel="stylesheet">
 
     <title>Brinque Feliz</title>
 
@@ -19,106 +19,52 @@
 </head>
 
 <body>
-    <div class="container">
-        <!-- <div class="row">
-            <div class="col"> -->
-                <!-- <div class="row justify-content-center">
-                    <div class="col-sm-6">
-                        <h1>Brinque Feliz</h1>
-                        <h2>Era uma vez uma grande loja...</h2>
-                    </div>
-                </div> -->
+    <div class="container h-100">
+        <div class="row h-100">
+            <div class="col-md-5 bg-light justify-content-center text-center">
 
-                <!-- <div class="row justify-content-center">
-                    <div class="col-6 col-sm-6"> -->
-                        <!--
-                        FORMULÁRIO PARA LOGAR
-                            -->
-                        <!-- <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">Entrar</h5>
-                                <form class="form-inline" method="post">
-                                    <input type="hidden" name="funcao" value="realizarLogin">
+                <div class="my-3 py-3">
+                    <h2>Brinque Feliz</h2>
+                    <p>Era uma vez uma grande loja...</p>
+                </div>
 
-                                    <div class="input-group mb-2">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text">@</div>
-                                        </div>
-                                        <label for="numeroId"><span class="glyphicon glyphicon-user"></span></label>
-                                        <input type="text" class="form-control" name="numeroId" id="numeroId"
-                                            placeholder="Número de Identificação">
-                                    </div>
+                <div class="card mb-4 shadow-sm mx-auto" style="width:18rem;" >
+                    <div class="card-body">
+                        <h5 class="card-title">Entrar</h5>
+                        <form class="form-inline" method="post">
+                            <input type="hidden" name="funcao" value="realizarLogin">
 
-                                    <div class="input-group mb-2">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text">@</div>
-                                        </div>
-                                        <label for="cpf"><span class="glyphicon glyphicon-lock"></span></label>
-                                        <input type="text" class="form-control" name="cpf" id="cpf" placeholder="CPF">
-                                    </div>
-
-                                    <input type="submit" class="btn btn-primary btn-block" value="Entrar">
-                                </form>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">@</div>
+                                </div>
+                                <label for="numeroId"><span class="glyphicon glyphicon-user"></span></label>
+                                <input type="text" class="form-control" name="numeroId" id="numeroId" placeholder="Número de Identificação">
                             </div>
-                        </div> -->
-<!-- 
-                    </div>
-                </div>
-            </div> -->
 
-            <!-- <div class="col">
-                <div class="col-sm-6">
-                    <img src="Recursos/child_playing.jpg" class="img-fluid " alt="IMAGEM DO BRINQUE FELIZ">
-                </div>
-            </div> -->
-        <!-- </div> -->
-
-        <div class="row justify-content-center">
-            <div class="d-md-flex flex-md-equal my-md-3 pl-md-3 justify-content-center w-100 h-100">
-
-                <div class="bg-light mr-md-3 px-md-5 text-center overflow-hidden ">
-                    <div class="my-3 py-3">
-                        <h2 class="display-5">Brinque Feliz</h2>
-                        <p class="lead">Era uma vez uma grande loja...</p>
-                    </div>
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Entrar</h5>
-                            <form class="form-inline" method="post">
-                                <input type="hidden" name="funcao" value="realizarLogin">
-
-                                <div class="input-group mb-2">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text">@</div>
-                                    </div>
-                                    <label for="numeroId"><span class="glyphicon glyphicon-user"></span></label>
-                                    <input type="text" class="form-control" name="numeroId" id="numeroId" placeholder="Número de Identificação">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">@</div>
                                 </div>
+                                <label for="cpf"><span class="glyphicon glyphicon-lock"></span></label>
+                                <input type="text" class="form-control" name="cpf" id="cpf" placeholder="CPF">
+                            </div>
 
-                                <div class="input-group mb-2">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text">@</div>
-                                    </div>
-                                    <label for="cpf"><span class="glyphicon glyphicon-lock"></span></label>
-                                    <input type="text" class="form-control" name="cpf" id="cpf" placeholder="CPF">
-                                </div>
-
-                                <input type="submit" class="btn btn-primary btn-block" value="Entrar">
-                            </form>
-                        </div>
+                            <input type="submit" class="btn btn-primary btn-block" value="Entrar">
+                        </form>
                     </div>
                 </div>
-
-                <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-                        <img src="Recursos/child_playing.jpg" class="img-fluid " alt="IMAGEM DO BRINQUE FELIZ">
-                </div>
-
             </div>
+
+            <div class="col-md-6 px-0 bg-dark"> 
+                <img src="Recursos/toy.jpg" class="h-100 img-fluid img-thumbnailrounded mx-auto d-block" alt="Nature" style="width:100%">            
+            </div>
+
+
         </div>
 
-
     </div>
-
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
