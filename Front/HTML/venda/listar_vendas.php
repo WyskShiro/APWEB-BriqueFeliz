f8241040f3cb01dd171090ec8173dbed8d4ababa
Rhebeca Abreu <rhebecaabreu@gmail.com>
2018-12-04 10:28:10 -0300

    Corrigindo erros merge

--- Front/HTML/venda/listar_vendas.php
+++ Front/HTML/venda/listar_vendas.php
@@ -86,6 +86,11 @@ $listaVenda = $this->vendaFactory->listar();
 </div>
 </div>
 
+<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+<!-- Icons -->
+<script>
+    feather.replace()
+</script>
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
